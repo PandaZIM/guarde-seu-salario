324fb5e0e8565fd86d4780c769f2f7365e739062
pandazim <jaotoledo74@gmail.com>
2020-11-01 18:40:49 -0400

    footer pdzim trademark

---
+++ src/components/madeBy.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function madeBy() {
+  return (
+    <div class="links">
+    <span>Made by: <a href="https://twitter.com/PandaZIM" target="_blank">PandaZIM</a></span>
+        <div class="more-links">
+            <a href="https://github.com/pandazim" target="_blank">
+                <img src="https://image.flaticon.com/icons/png/512/25/25231.png" alt="GitHub"/>
+            </a>
+        </div>
+    </div>
+  );
+}
+
+export default madeBy;
